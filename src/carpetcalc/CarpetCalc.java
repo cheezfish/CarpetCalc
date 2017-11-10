b14a4ab4db83d8da298161b2c2b9c1f4ac9720d2
Imran <Imran@192.168.0.14>
2017-11-10 20:39:32 +0000

    commit

--- src/carpetcalc/CarpetCalc.java
+++ src/carpetcalc/CarpetCalc.java
@@ -6,7 +6,8 @@ import java.util.Scanner; //allows me to make use of the scanner function
  *
  * @author Imran Aziz 27/09/2017 18:41
  */
-public class CarpetCalc {
+public class CarpetCalc 
+{
     
     public static double width;
     public static double length;
@@ -15,50 +16,37 @@ public class CarpetCalc {
     public static double carpet;
     public static double carpround;//I have now specified all of my variables (in this case doubles)
     
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         
         measurewidth();
         measurelength();
         carpet();
         carpround(); //this is the order in which the program will execute my methods
+        System.exit(0);
         
     }
 
-    public static void measurewidth() { //here I'll allow input for the room width
+    public static void measurewidth() 
+    { //here I'll allow input for the room width
         
-        double width = 0;
-        System.out.println("Enter width (in meters)");
         Scanner scanner1 = new Scanner(System.in);
-        try
-            { //blocks answers that aren't integers
-                width = Double.parseDouble(scanner1.nextLine());
-            }
-            catch(IllegalArgumentException ex) 
-            {
-                System.out.println("Nope, use a number, and a sensible one at that");
-                measurewidth();
-            }
+        System.out.println("Enter width (in meters)");
+        width = Double.parseDouble(scanner1.nextLine()); 
         
     }
     
-    public static void measurelength() { //see above, but length
+    public static void measurelength() 
+    { //see above, but length
         
-        double length = 0;
-        System.out.println("Enter length (in meters)");
         Scanner scanner2 = new Scanner(System.in);
-        try
-            { //blocks answers that aren't integers
-                length = Double.parseDouble(scanner2.nextLine());
-            }
-            catch(IllegalArgumentException ex) 
-            {
-                System.out.println("Nope, use a number, and a sensible one at that");
-                measurelength();
-            }
+        System.out.println("Enter length (in meters)");
+        length = Double.parseDouble(scanner2.nextLine());
         
     }
     
-    public static void carpet() { //the carpet must allow for wastage, this method accounts for that
+    public static void carpet() 
+    { //the carpet must allow for wastage, this method accounts for that
         
         area=length*width;
         wastage=(area*0.05);
@@ -66,9 +54,10 @@ public class CarpetCalc {
         
     }
     
-    public static void carpround() { //this method rounds the carpet size value to 1dp
+    public static void carpround() 
+    { //this method rounds the carpet size value to 1dp
         
-        carpround=Math.round(10*carpet);
+        carpround=Math.floor(10*carpet); //rounds down
         System.out.println("Your room is " + area + "m^2");
         System.out.println("The extra you need for wastage is " + wastage + " m^2");
         System.out.println("Your carpet is approximately " + carpround/10 + " m^2");
